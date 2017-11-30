ccceb1302e2504ca500c504a41e14af95a85f30b
Jordan Blakey <jordan@jordanblakey.com>
2017-11-30 01:39:02 -0600

    1-filter

---
+++ 1-filter.js
@@ -0,0 +1,30 @@
+let animals = [
+  {name: 'Fluffykins', species: 'rabbit' },
+  {name: 'Caro', species: 'dog' },
+  {name: 'Hamilton', species: 'rabbit'},
+  {name: 'Harold', species: 'dog'},
+  {name: 'Ursula', species: 'dog'},
+  {name: 'Jimmy', species: 'dog'}
+]
+
+
+// FILTER USING IMPERATIVE METHOD
+// More logic, harder ro reason about, less readable, doesn't scale well.
+let dogs = []
+for (let i = 0; i < animals.length; i++) {
+  if (animals[i].species === 'dog') dogs.push(animals[i])
+}
+let notDogs = []
+for (let i = 0; i < animals.length; i++) {
+  if (animals[i].species !== 'dog') dogs.push(animals[i])
+}
+
+
+// FILTER USING FUNCTIONAL METHOD
+// Small, single-responsibility, composable, reusable functions.
+let isDog = (animal) => {return animal.species === 'dog'}
+let dogs2 = animals.filter(isDog)
+let notDogs2 = animals.reject(isDog)
+
+
+console.log(dogs, dogs2, notDogs, notDogs2)
